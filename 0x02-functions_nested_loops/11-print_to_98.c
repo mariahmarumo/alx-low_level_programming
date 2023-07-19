c47c26241d95874b9835ff38faa30e61849d6048
Dimpho <mariahmarumo@gmail.com>
2023-07-18 19:57:39 -0700

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -13,9 +13,9 @@ if (n <= 98)
 for (i = n; i <= 98; i++)
 {
 if (i != 98)
-printf("% d, ", i);
+printf("%d, ", i);
 else if (i == 98)
-printf("% d\n", i);
+printf("%d\n", i);
 }
 }
 else if (n >= 98)
@@ -23,9 +23,9 @@ else if (n >= 98)
 for (j = n; j >= 98; j--)
 {
 if (j != 98)
-printf("% d, ", j);
+printf("%d, ", j);
 else if (j == 98)
-printf("% d\n", j);
+printf("%d\n", j);
 }
 }
 }
