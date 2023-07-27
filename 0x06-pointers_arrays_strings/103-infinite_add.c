56bbad62446ee7bba9ff99c79f7eb82c7971c6d7
Dimpho <mariahmarumo@gmail.com>
2023-07-27 11:13:28 -0700

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -8,22 +8,22 @@
 
 void rev_string(char *n)
 {
-	int i = 0;
-	int j = 0;
-	char temp;
+int i = 0;
+int j = 0;
+char temp;
 
-	while (*(n + i) != '\0')
-	{
-		i++;
-	}
-	i--;
+while (*(n + i) != '\0')
+{
+i++;
+}
+i--;
 
-	for (j = 0; j < i; j++, i--)
-	{
-		temp = *(n + j);
-		*(n + j) = *(n + i);
-		*(n + i) = temp;
-	}
+for (j = 0; j < i; j++, i--)
+{
+temp = *(n + j);
+*(n + j) = *(n + i);
+*(n + i) = temp;
+}
 }
 
 /**
@@ -53,11 +53,11 @@ while (j >= 0 || i >= 0 || overflow == 1)
 if (i < 0)
 val1 = 0;
 else
-val1 = *(n1 + i) - 0;
+val1 = *(n1 + i) -0;
 if (j < 0)
 val2 = 0;
 else
-val2 = *(n2 + j) - 0;
+val2 = *(n2 + j) -0;
 temp_tot = val1 + val2 + overflow;
 if (temp_tot >= 10)
 overflow = 1;
